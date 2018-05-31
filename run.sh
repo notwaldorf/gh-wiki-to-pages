7e079672a7e51520e2eeafee431edc504b24deb5
Monica Dinculescu <noms@google.com>
2018-05-31 16:23:39 -0700

    add script

--- run.sh
+++ run.sh
@@ -1,39 +1,32 @@
 #!/bin/bash -e
 
-# This script pushes a demo-friendly version of your element and its
-# dependencies to gh-pages.
-
-# A gh-pages branch needs to exist, so before you run this script for the
-# first time ever, make sure you ran
-#     git branch -b gh-pages
-#     git push --set-upstream origin gh-pages
-# You don't ever need to run this again.
-
 # Run in a clean directory passing in a GitHub org and repo name
 org="notwaldorf"
 repo="test-gh-pages-script"
 folder="_posts"
 
-# make folder (same as input, no checking!)
+# make folder (no checking!)
 rm -rf $repo
 mkdir $repo
+echo "cloning... $repo"
 git clone https://github.com/$org/$repo.git --single-branch
 
 # switch to gh-pages branch
+echo "switching to gh-pages branch..."
 pushd $repo >/dev/null
 git checkout --orphan gh-pages
 
-rm .gitignore
-echo '*' >> .gitignore          # ignore everything
-echo '!_layouts/' >> .gitignore  # except for these jekyll things
-echo '!_posts/' >> .gitignore
-echo '!_config.yml' >> .gitignore
-echo '!index.html' >> .gitignore
+# uncommit everything
+echo "uncommiting everything..."
+git rm -rf --cached --ignore-unmatch *
 
 ## copy everything from the docs-site in here
+echo "copying jekyll folders..."
+mv docs-site/_layouts/ ./_layouts
 cp -R docs-site/* .
 
 ## clone the wiki in here
+echo "clning wiki..."
 git clone https://github.com/$org/$repo.wiki.git --single-branch $folder
 
 # remove the .git folder from the wiki repo
@@ -51,8 +44,15 @@ cd ..
 
 # copy everything from the docs
 # send it all to github
-git add -A .
-git commit -am 'deploy to gh-pages'
+echo "git add-ing..."
+git add _layouts
+git add _posts
+git add _config.yml
+git add index.html
+
+git commit -am 'deployed to gh-pages'
 git push -u origin gh-pages --force
+
+echo "done and deployed!"
 
 popd >/dev/null
