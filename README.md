d03c5d47f2ed7c94861425848d3dd7016391fcf0
Monica Dinculescu <noms@google.com>
2018-05-31 17:23:35 -0700

    sorta style the site

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # test-gh-pages-script
+
+## Test the site locally
+
+```
+cd docs_site
+sh fetch-wiki.sh
+jekyll serve --watch
+```
--- docs-site/fetch-wiki.sh
+++ docs-site/fetch-wiki.sh
@@ -14,10 +14,15 @@ rm -rf _posts/.git
 # YEAR-MONTH-DAY-title.md
 echo "renaming wiki pages..."
 cd _posts
+mkdir temp
 for file in *.md
 do
+  # prepend the post type
+  echo '---\nlayout: post\n---' | cat - $file > temp/$file && mv temp/$file $file
+
   mv "$file" "2018-05-31-${file}"
 done
+rm -rf temp
 cd ..
 
 echo "done!"
